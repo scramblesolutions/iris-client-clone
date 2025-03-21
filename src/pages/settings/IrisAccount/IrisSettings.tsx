8ad23a8519fb4c6b99209d39013da51975a38c32
jaskarandeogangit <jaskaran2k15@gmail.com>
2025-03-21 13:16:20 -0700

    add theme customizations

--- src/pages/settings/IrisAccount/IrisSettings.tsx
+++ src/pages/settings/IrisAccount/IrisSettings.tsx
@@ -3,7 +3,7 @@ import IrisAccount from "./IrisAccount"
 function IrisSettings() {
   return (
     <div>
-      <h1 className="text-2xl mb-4">Iris.to username</h1>
+      <h1 className="text-2xl mb-4">21Eyes username</h1>
       <div className="flex flex-col gap-4 prose">
         <div>
           <IrisAccount />
